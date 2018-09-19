3217b64ee2a1b61669925092404f2a9fede425f9
David <david@Davids-MacBook-Air.local>
2018-09-19 16:08:58 -0700

    updated

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Booking from './components/booking.jsx';
 
-// ReactDOM.render(
-//   React.createElement(Booking, {url: window.location.pathname.match(/[0-9]+/g)[0];}, null)
-// , document.getElementById('booking'));
+ReactDOM.render(
+  React.createElement(Booking, {homeId: window.location.pathname.match(/[0-9]+/g)[0]}, null)
+, document.getElementById('booking'));
 
 window.Booking = Booking;
